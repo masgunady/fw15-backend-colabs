d53bdeca2c615b6ffa3dd2927b06fe5d633d5877
ardazh <127732352+ardazh@users.noreply.github.com>
2023-05-24 02:35:21 +0700

    fix: userModel & auth fucn by postman

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -35,15 +35,6 @@ exports.findOneByEmail = async function(email){
     return rows[0]
 }
 
-exports.findOneByEmail = async function(email){
-    const query = `
-  SELECT * FROM "users" WHERE email=$1
-  `
-    const values = [email]
-    const {rows} = await db.query(query, values)
-    return rows[0]
-}
-
 
 exports.insert = async function(data){
     const query = `
