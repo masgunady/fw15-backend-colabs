37361effacda6ade5001d9d7e2c210c7680a4a26
Yogi Prayoga <yogiprayoga1333@gmail.com>
2023-05-23 22:10:35 +0700

    Add new models end poin, users models

---
+++ src/models/user.model.js
@@ -0,0 +1,62 @@
+const db = require ("../helpers/db.helper")
+
+
+exports.findAllUsers = async function(page, limit, search, sort, sortBy){
+    page = parseInt(page) || 1
+    limit = parseInt(limit) || 5
+    search = search || ""
+    sort = sort || "id"
+    sortBy = sortBy || "ASC"
+
+    const offset = (page - 1) * limit
+
+    const query = `
+    SELECT * FROM "users" WHERE "email" LIKE $3 ORDER BY "${sort}" ${sortBy} LIMIT $1  OFFSET $2 
+    `
+    const values = [limit, offset,`%${search}%`]
+    const {rows} = await db.query(query, values)
+    return rows
+}
+
+exports.findOne = async function(id){
+    const query = `
+  SELECT  * FROM "users" WHERE id=$1
+  `
+    const values = [id]
+    const {rows} = await db.query(query, values)
+    return rows[0]
+}
+
+
+exports.insert = async function(data){
+    const query = `
+  INSERT INTO "users" ("email", "password") 
+  VALUES ($1, $2) RETURNING *
+  `
+    const values = [data.email, data.password]
+    const {rows} = await db.query(query, values)
+    return rows [0]
+}
+
+exports.update = async function(id, data){
+    const query = `
+  UPDATE "users" 
+  SET 
+  "email"=COALESCE(NULLIF($2, ''), "email"),
+  "password"=COALESCE(NULLIF($3, ''), "password")
+  WHERE "id"=$1
+  RETURNING *
+`
+    const values = [id, data.email, data.password]
+    const {rows} = await db.query(query, values)
+    return rows [0]
+}
+
+exports.destroy = async function(id){
+    const query = `
+  DELETE FROM "users" WHERE "id"=$1
+`
+    const values = [id]
+    const {rows} = await db.query(query, values)
+    return rows [0]
+}
