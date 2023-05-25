07470255f9eb1b4b16e00b90f342eb47244d8150
Yogi Prayoga <yogiprayoga1333@gmail.com>
2023-05-25 15:18:22 +0700

    Update: add new colom commnet userId and join tabel profiles to tabele commnets

--- src/models/comments.model.js
+++ src/models/comments.model.js
@@ -1,6 +1,6 @@
-const db =  require("../helpers/db.helper")
+const db = require("../helpers/db.helper")
 
-exports.findAllComments = async function(page, limit, search, sort, sortBy){
+exports.findAllComments = async function (page, limit, search, sort, sortBy) {
     page = parseInt(page) || 1
     limit = parseInt(limit) || 5
     search = search || ""
@@ -10,37 +10,43 @@ exports.findAllComments = async function(page, limit, search, sort, sortBy){
     const offset = (page - 1) * limit
 
     const query = `
-  SELECT * FROM "comments" WHERE "name" LIKE $3 ORDER BY "${sort}" ${sortBy} LIMIT $1  OFFSET $2 
+    SELECT c.*, "p"."username" AS "username", "p"."picture" AS "picture"
+    FROM "comments" c
+    JOIN "profiles" "p" ON "c"."userId" = "p"."userId"
+  WHERE "name" LIKE $3 ORDER BY "${sort}" ${sortBy} LIMIT $1  OFFSET $2 
   `
-    const values = [limit, offset,`%${search}%`]
-    const {rows} = await db.query(query, values)
+    const values = [limit, offset, `%${search}%`]
+    const { rows } = await db.query(query, values)
     return rows
 }
 
-exports.findOne = async function(id){
+exports.findOne = async function (userId) {
     const query = `
-SELECT  * FROM "comments" WHERE id=$1
+    SELECT c.*, "p"."username" AS "username", "p"."picture" AS "picture"
+    FROM "comments" c
+    JOIN "profiles" "p" ON "c"."userId" = "p"."userId"
+    WHERE "c".id = $1
 `
-    const values = [id]
-    const {rows} = await db.query(query, values)
+    const values = [userId]
+    const { rows } = await db.query(query, values)
     return rows[0]
 }
 
-exports.insert = async function(data){
+exports.insert = async function (data) {
     const query = `
-INSERT INTO "comments" ("name", "email", "articleId) 
-VALUES ($1, $2, $3) RETURNING *
+INSERT INTO "comments" ("name", "userId", "articleId", "content") 
+VALUES ($1, $2, $3, $4) RETURNING *
 `
-    const values = [data.name, data.email, data.articleId]
-    const {rows} = await db.query(query, values)
-    return rows [0]
+    const values = [data.name, data.userId, data.articleId, data.content]
+    const { rows } = await db.query(query, values)
+    return rows[0]
 }
 
-exports.destroy = async function(id){
+exports.destroy = async function (id) {
     const query = `
 DELETE FROM "comments" WHERE "id"=$1
 `
     const values = [id]
-    const {rows} = await db.query(query, values)
-    return rows [0]
+    const { rows } = await db.query(query, values)
+    return rows[0]
 }
